311cd0da4d03e9a0d09a4117c4c923e5446c6baa
username <wecp-dummy@gmail.com>
2025-01-08 12:32:09 +0000

    wecp initial commit

---
+++ backend/src/main/java/com/wecp/progressive/exception/TeamAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.wecp.progressive.exception;
+
+public class TeamAlreadyExistsException extends RuntimeException {
+    public TeamAlreadyExistsException(String message){
+        super(message);
+    }
+}
